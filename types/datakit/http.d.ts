4a51a51b12f53c7193bb4c9d4aa8c56eb84d0fff
Jordan Fowler <me@jordanfowler.co>
2023-09-02 04:22:57 -0600

    Added more types.

--- types/datakit/http.d.ts
+++ types/datakit/http.d.ts
@@ -1,12 +1,31 @@
+export type HttpMethod = 'HEAD' | 'GET' | 'OPTIONS' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
+
 export interface Params {
     headers: { [key: string]: string };
 }
 
+export interface URL {
+    scheme: string;
+    opaque?: string;
+    user?: Userinfo;
+    host: string;
+    path: string;
+    rawPath?: string;
+    forceQuery?: boolean;
+    rawQuery?: string;
+    fragment?: string;
+}
+
+export interface Userinfo {
+    username: string;
+    password?: string;
+}
+
 export interface Request {
-    method: string;
+    method: HttpMethod;
     url: string;
     body: string;
-    headers: { [key: string]: string[] };
+    headers: { [key: string]: string };
 }
 
 export interface Response {
@@ -19,19 +38,18 @@ export interface Response {
     error: string;
     errorCode: number;
     request: Request | null;
+    json(): any;
 }
 
-export type HttpMethod = 'HEAD' | 'GET' | 'OPTIONS' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
-
 declare namespace http {
-    function head(url: string, params?: Params): Response;
-    function get(url: string, params?: Params): Response;
-    function options(url: string, body: any, params?: Params): Response;
-    function post(url: string, body: any, params?: Params): Response;
-    function put(url: string, body: any, params?: Params): Response;
-    function patch(url: string, body: any, params?: Params): Response;
-    function del(url: string, body: any, params?: Params): Response;
-    function request(method: HttpMethod, url: string, body?: any, params?: Params): Response;
+    function head(url: string | URL, params?: Params): Response;
+    function get(url: string | URL, params?: Params): Response;
+    function options(url: string | URL, body: any, params?: Params): Response;
+    function post(url: string | URL, body: any, params?: Params): Response;
+    function put(url: string | URL, body: any, params?: Params): Response;
+    function patch(url: string | URL, body: any, params?: Params): Response;
+    function del(url: string | URL, body: any, params?: Params): Response;
+    function request(method: HttpMethod, url: string | URL, body?: any, params?: Params): Response;
 }
 
 export default http;
